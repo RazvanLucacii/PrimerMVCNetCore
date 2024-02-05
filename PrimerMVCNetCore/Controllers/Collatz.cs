4f88a37057ca18f0485bbc78b455288eaa5329a6
RazvanLucacii <rlucaci48@gmail.com>
2024-02-05 14:20:49 +0100

    1

---
+++ PrimerMVCNetCore/Controllers/Collatz.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMVCNetCore.Controllers
+{
+    public class Collatz : Controller
+    {
+        [HttpGet]
+        public IActionResult ConjeturaCollatzForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConjeturaCollatzForm(int numero)
+        {
+            List<int> numeros = new List<int>();
+            while (numero != 1)    
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                numeros.Add(numero);
+            }
+            //devolvemos el model a la vista
+            return View(numeros);
+        }
+    }
+}
---
+++ PrimerMVCNetCore/Controllers/Matematicas.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMVCNetCore.Controllers
+{
+    public class Matematicas : Controller
+    {
+        [HttpGet]
+        public IActionResult SumarNumerosPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SumarNumerosGet(int Numero1, int Numero2)
+        {
+            for (int i = 0; i < Numero2; i++)
+            {
+                ViewData[i.ToString()] = Numero1;
+            }
+            ViewBag.Numero1 = Numero1;
+            ViewBag.Numero2 = Numero2;
+            ViewData["DATOSSUMA"] = Numero1 + Numero2;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TablaMultiplicarSimple()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TablaMultiplicarSimple(int numero)
+        {
+            List<int> numeros = new List<int>();
+            while ()
+            {
+                
+            }
+            return View(numeros);
+        }
+    }
+}
